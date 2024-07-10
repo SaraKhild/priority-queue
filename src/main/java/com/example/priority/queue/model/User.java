5201d3b6d3f898ce6f6b22dbd8f0e51d9fdad8be
saraalhosin <SKAlhosin@baraq.com>
2024-07-10 10:25:44 +0300

    modify the model

--- src/main/java/com/example/priority/queue/model/User.java
+++ src/main/java/com/example/priority/queue/model/User.java
@@ -1,24 +1,17 @@
 package com.example.priority.queue.model;
 
 import com.example.priority.queue.enums.Category;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    private int id;
-    private Category category;
-
-      @JsonCreator
-    public User(@JsonProperty("id") int id, @JsonProperty("category") Category category) {
-        this.id = id;
-        this.category = category;
-    }
-
+  private int id;
+  private Category category;
 
 }
